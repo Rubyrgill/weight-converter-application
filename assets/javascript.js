afd494f5b016abe22f062f3db1b156a84d089a76
RubyG <rubygillr@gmail.com>
2018-07-12 11:07:08 -0400

    Javascript complete

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,11 +1,20 @@
-//GLOBAL VARIABLES
-
 
 //FUNCTIONS
+
+//Grabbing the input id, and adding eventlistener
 document.getElementById("lbs").addEventListener('input', function () {
+
+    //creating a variable to store the value entered in input
     var pounds = event.target.value;
-    console.log(pounds);
+    //calculating conversation and displaying in HTML
+    //GRAMS
+    document.getElementById("gramsOut").innerHTML =
+        pounds / 0.0022046;
+    //KILOGRAMS
+    document.getElementById("kgOut").innerHTML =
+        pounds / 2.2046;
+    //OUNCES
+    document.getElementById("ounceOut").innerHTML =
+        pounds * 16;
 })
 
-
-//PROCESSES
